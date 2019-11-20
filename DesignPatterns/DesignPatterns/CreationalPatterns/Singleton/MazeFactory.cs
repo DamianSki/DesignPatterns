aa8d5e5d210a578e49fc255705fbfd504d18337d
Damian Sobanski <damian.flexsoft@gmail.com>
2019-11-20 12:17:46 +0100

    Singleton design pattern implemented

---
+++ DesignPatterns/DesignPatterns/CreationalPatterns/Singleton/MazeFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.CreationalPatterns.Singleton
+{
+    public sealed class MazeFactory
+    {
+        private MazeFactory() { }
+
+        private static readonly Lazy<MazeFactory> _instance = new Lazy<MazeFactory>(() => new MazeFactory(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+        public static MazeFactory Instance => _instance.Value;
+    }
+}
