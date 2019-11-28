0bd2611f3d9e1524e9829d64fe44b7c8022d9769
Damian Sobanski <damian.flexsoft@gmail.com>
2019-11-28 20:34:52 +0100

    Proxy pattern added

---
+++ DesignPatterns/DesignPatterns/Structural/Proxy/Proxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignPatterns.Structural.Proxy
+{
+    public class Proxy : ISubject
+    {
+        private ISubject _realSubject;
+
+        public Proxy(ISubject realSubject)
+        {
+            _realSubject = realSubject;
+        }
+                
+        public void Request()
+        {
+            if (CheckAccess())
+            {
+                _realSubject = new RealSubject();
+                _realSubject.Request();
+
+                LogAccess();
+            }
+        }
+
+        public bool CheckAccess()
+        {            
+            Console.WriteLine("Checking access prior to firing a real request.");
+            return true;
+        }
+
+        public void LogAccess()
+        {
+            Console.WriteLine("Logging the time of request.");
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Proxy/RealSubject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Structural.Proxy
+{
+    public class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("Real subject got request!");
+        }
+    }
+}
