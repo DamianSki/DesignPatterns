e315b0c70fc346257740b1a2b97f0aa569c4bc9a
Damian Sobanski <damian.flexsoft@gmail.com>
2019-12-01 13:39:40 +0100

    Template design pattern

---
+++ DesignPatterns/DesignPatterns/Behavioral/Template/AbstractClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Template
+{
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            BaseOperation1();
+            BaseOperation2();
+        }
+        protected void BaseOperation1() => Console.WriteLine($"This opration: {nameof(BaseOperation1)} cannot be overriden");
+        protected void BaseOperation2() => Console.WriteLine($"This opration: {nameof(BaseOperation2)} cannot be overriden");
+        protected abstract void RequiredOperation1();
+        protected abstract void RequiredOperation2();
+        protected virtual void Hook1() { }
+        protected virtual void Hook2() { }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Behavioral/Template/ConcreteClassA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Template
+{
+    public class ConcreteClassA : AbstractClass
+    {
+        protected override void RequiredOperation1() => Console.WriteLine($"Overriden abstract operation: {nameof(RequiredOperation1)}");
+        protected override void RequiredOperation2() => Console.WriteLine($"Overriden abstract operation: {nameof(RequiredOperation2)}");
+    }
+}
